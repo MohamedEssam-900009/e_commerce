f34a25e297613b28382fae3f0705a7b46c3ee7c7
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-07-12 14:26:17 +0200

    [ENHANCMENT] add the landing page to save the state of logged in user

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,12 +1,32 @@
+import 'package:e_commerce/controllers/auth_controller.dart';
+import 'package:e_commerce/services/auth.dart';
+import 'package:e_commerce/views/pages/auth_page.dart';
+import 'package:e_commerce/views/pages/bottom_navbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({ Key? key }) : super(key: key);
+  const LandingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final user = snapshot.data;
+          if (user == null) {
+            return ChangeNotifierProvider<AuthController>(
+              create: (context) => AuthController(auth: auth),
+              child: const AuthPage(),
+            );
+          }
+          return const BottomNavbar();
+        }
+      return BottomNavbar();
+      },
     );
   }
 }
